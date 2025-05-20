dbbf190e1752f13cb2416c22644265a88b2f1125
AntonioALino <antonioaplino@gmail.com>
2025-05-20 10:43:28 -0300

    :white_check_mark: Services feitos

---
+++ src/services/Posts/PostsServices.ts
@@ -0,0 +1,72 @@
+import { prisma } from "../../database/Prisma"
+
+export const createPost = async (title: string, content: string, userId: string, image: string) => {
+    return await prisma.post.create({
+        data: {
+            title,
+            content,
+            slug: title.toLowerCase().replace(/\s+/g, '-'), 
+            image: new Uint8Array(Buffer.from(image, 'base64')),
+            author: {
+                connect: { id: userId }
+            }
+        }
+    })
+}
+
+export const getPostById = async (id : string) => {
+    return await prisma.post.findUnique({
+        where: {
+            id
+        }
+    })
+}
+
+export const getAllPosts = async () => {
+    return await prisma.post.findMany({
+        select: {
+            title: true,
+            content: true,
+            author: {
+                select: {
+                    name: true,
+                    email: true
+                }
+            },
+            createdAt: true,
+        }
+    })
+}
+
+export const getPostsByUserId = async (userId: string) => {
+    return await prisma.post.findMany({
+        where: {
+            authorId: userId
+        },
+        select: {
+            title: true,
+            content: true,
+            createdAt: true,
+        }
+    })
+}
+
+export const updatePost = async (id: string, title: string, content: string) => {
+    return await prisma.post.update({
+        where: {
+            id
+        },
+        data: {
+            title,
+            content
+        }
+    })
+}
+
+export const deletePost = async (id: string) => {
+    return await prisma.post.delete({
+        where: {
+            id
+        }
+    })
+}
